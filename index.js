55432d3fb05fca4fc5f763094f8afb51aaeb720d
Seth Klimpsch <evangeling.klimpsch@gmail.com>
2024-04-04 09:33:58 +1100

    Changed fetch to be a workable function 👍

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-fetch('https://openlibrary.org/search.json?q=the+lord+of+the+rings')
-    .then(response => response.json())
-    .then(data => console.log(data.numFound))
-    .catch(error => console.error(error));
-
-fetch('https://openlibrary.org/search.json?q=the+lord+of+the+rings')
-    .then(response => response.json())
-    .then(data => console.log(data.cover_i))
-    .catch(error => console.error(error));
+async function fetchBook(){
+    const response = await 
+fetch('https://openlibrary.org/search.json?q=the+lord+of+the+rings');
+    const search = await response.json();
+    console.log(search);
+}
+fetchBook()
