2b08eeb5c195f4442dc0c14b3337ad975b71b7db
Seth Klimpsch <evangeling.klimpsch@gmail.com>
2024-04-03 16:43:50 +1100

    Successfully fetching openLibrary API data

---
+++ index.js
@@ -0,0 +1,4 @@
+fetch('https://openlibrary.org/search.json?q=the+lord+of+the+rings')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
