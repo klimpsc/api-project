9336cc5fa93da1bf6cf6d94263116c3456052e05
Seth Klimpsch <evangeling.klimpsch@gmail.com>
2024-04-04 10:26:44 +1100

    Tidy search, and make beginning on cards for book data display

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 async function fetchData() {
-    const response = await fetch("https://openlibrary.org/search.json?q=the+lord+of+the+rings");
+    const response = await fetch("https://openlibrary.org/search.json?q=${}");
     const books = await response.json();
     console.log(books);
-  }
-fetchData()
+}
