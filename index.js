2c42494693eafeeb04fe882ffc18edcaa69fd3f6
Seth Klimpsch <evangeling.klimpsch@gmail.com>
2024-04-04 09:57:55 +1100

    reverted back to previous js function. May need to learn more about using this api.

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-async function fetchData(){
-
-    try{
-        const bookName = document.getElementById("bookName").value.toLowerCase();
-        const response = await fetch('https://openlibrary.org/search.json?q=${}')
-
-    }
-}
+async function fetchData() {
+    const response = await fetch("https://openlibrary.org/search.json?q=the+lord+of+the+rings");
+    const books = await response.json();
+    console.log(books);
+  }
 fetchData()
