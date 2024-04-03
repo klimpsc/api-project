eb506fd2c686edefdd40082291339ad2d29cd01b
Seth Klimpsch <evangeling.klimpsch@gmail.com>
2024-04-03 17:35:52 +1100

    Experimenting while trying to retrieve cover data

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 fetch('https://openlibrary.org/search.json?q=the+lord+of+the+rings')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => console.log(data.numFound))
+    .catch(error => console.error(error));
+
+fetch('https://openlibrary.org/search.json?q=the+lord+of+the+rings')
+    .then(response => response.json())
+    .then(data => console.log(data.cover_i))
     .catch(error => console.error(error));
