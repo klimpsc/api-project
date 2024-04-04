52e39ce1f9ca3a0c575db8c8d93cbb1f373b1007
Seth Klimpsch <evangeling.klimpsch@gmail.com>
2024-04-04 16:33:11 +1100

    fetching new (more friendly) API

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 async function fetchData() {
-    const response = await fetch("");
-    const # = await response.json();
-    console.log(#);
+    const response = await fetch("https://www.dnd5eapi.co/api/${}");
+    const data = await response.json();
+    console.log(data);
 }
