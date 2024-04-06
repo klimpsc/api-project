2e12839876e851982213fab1275e5dff8a580a71
Seth Klimpsch <evangeling.klimpsch@gmail.com>
2024-04-06 13:25:31 +1100

    Successfully cleaned the js after fetching new API

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-async function fetchData() {
-    const response = await fetch("https://www.dnd5eapi.co/api/${}");
-    const data = await response.json();
-    console.log(data);
-}
+fetch('https://www.dnd5eapi.co/api/spells/acid-arrow')
+    .then(res => {
+        if (res.ok) {
+            console.log('success');
+            return res.json();
+        }   else {
+            console.log('not successful')
+        }
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log('ERROR'))
