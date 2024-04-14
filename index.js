086948ed8b1ed712e9cc256a441188cb3c8a1311
Seth Klimpsch <evangeling.klimpsch@gmail.com>
2024-04-15 08:53:15 +1000

    replaced equiptment with features, and added onclick to display the content on the terminal. Need to now display content on the page

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 let races_api_url = "https://www.dnd5eapi.co/api/races"
 let classes_api_url = "https://www.dnd5eapi.co/api/classes"
 let monsters_api_url = "https://www.dnd5eapi.co/api/monsters"
-let equiptment_api_url = "https://www.dnd5eapi.co/api/equiptment"
 let spells_api_url = "https://www.dnd5eapi.co/api/spells"
-
-// go through and allocate with correct cards and have images in cards with 
-// correct images fetched from their own aligned contect 
+let features_api_url = "https://www.dnd5eapi.co/api/features"
 
 async function racesData() {
     const response = await fetch(races_api_url);
@@ -25,14 +22,14 @@ async function monsterData() {
     console.log(data.results);
 }
 
-async function equiptmentData() {
-    const response = await fetch(equipment_api_url);
+async function spellsData() {
+    const response = await fetch(spells_api_url);
     const data = await response.json();
     console.log(data.results);
 }
 
-async function spellsData() {
-    const response = await fetch(spells_api_url);
+async function featuresData() {
+    const response = await fetch(features_api_url);
     const data = await response.json();
     console.log(data.results);
 }
