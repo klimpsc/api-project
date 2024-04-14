9529da0a465f2e7ce61370ded65a11438c51731d
Seth Klimpsch <evangeling.klimpsch@gmail.com>
2024-04-15 09:54:03 +1000

    rewrote fetch data etc, returns only promise.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-let data_url = "https://www.dnd5eapi.co/api/races"
- 
+
+async function fetchData(){
+    try {
+        const response = await fetch ("https://www.dnd5eapi.co/api/races");
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('ERROR!! ERROR!!', error);
+    }
+} 
