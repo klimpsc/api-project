6dcb6802cd23594fdeb4088ea37b98ec021f2ae7
Seth Klimpsch <evangeling.klimpsch@gmail.com>
2024-04-14 17:45:19 +1000

    Fiddling with card temporary images. Also will need to find replacements to put in images folder (the api wouldnt have an image for the catagories 😅

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ let monsters_api_url = "https://www.dnd5eapi.co/api/monsters"
 let equiptment_api_url = "https://www.dnd5eapi.co/api/equiptment"
 let spells_api_url = "https://www.dnd5eapi.co/api/spells"
 
+// go through and allocate with correct cards and have images in cards with 
+// correct images fetched from their own aligned contect 
+
 async function racesData() {
     const response = await fetch(races_api_url);
     const data = await response.json();
@@ -13,31 +16,23 @@ async function racesData() {
 async function classesData() {
     const response = await fetch(classes_api_url);
     const data = await response.json();
-    console.log(data.name);
-    console.log(data.hit_die);
-    console.log(data.proficiencies[4].name);
+    console.log(data.results);
 }
 
 async function monsterData() {
     const response = await fetch(monsters_api_url);
     const data = await response.json();
-    console.log(data.name);
-    console.log(data.hit_die);
-    console.log(data.proficiencies[4].name);
+    console.log(data.results);
 }
 
 async function equiptmentData() {
     const response = await fetch(equipment_api_url);
     const data = await response.json();
-    console.log(data.name);
-    console.log(data.hit_die);
-    console.log(data.proficiencies[4].name);
+    console.log(data.results);
 }
 
 async function spellsData() {
     const response = await fetch(spells_api_url);
     const data = await response.json();
-    console.log(data.name);
-    console.log(data.hit_die);
-    console.log(data.proficiencies[4].name);
+    console.log(data.results);
 }
