7a8234995c831e7a31f21c4c7d8c34a1d61ce725
Seth Klimpsch <evangeling.klimpsch@gmail.com>
2024-04-15 10:19:51 +1000

    tidy

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-let races_api_url = "https://www.dnd5eapi.co/api/races"
-let classes_api_url = "https://www.dnd5eapi.co/api/classes"
-let monsters_api_url = "https://www.dnd5eapi.co/api/monsters"
-let equiptment_api_url = "https://www.dnd5eapi.co/api/equiptment"
-let spells_api_url = "https://www.dnd5eapi.co/api/spells"
-
-let features_api_url = "https://www.dnd5eapi.co/api/features"
+let races_url = "https://www.dnd5eapi.co/api/races"
+let classes_url = "https://www.dnd5eapi.co/api/classes"
+let monsters_url = "https://www.dnd5eapi.co/api/monsters"
+let spells_url = "https://www.dnd5eapi.co/api/spells"
+let features_url = "https://www.dnd5eapi.co/api/features"
 
 async function racesData() {
-    const response = await fetch(races_api_url);
-async function monsterData() {
-    console.log(data.results);
-}}
-
-async function equiptmentData() {
-    const response = await fetch(equipment_api_url);
-async function spellsData() {
-    const response = await fetch(spells_api_url);
+    const response = await fetch(races_url);
     const data = await response.json();
     console.log(data.results);
-}}
-
+}
+async function classesData() {
+    const response = await fetch(classes_url);
+    const data = await response.json();
+    console.log(data.results);
+}
 async function spellsData() {
-    const response = await fetch(spells_api_url);
+    const response = await fetch(spells_url);
+    const data = await response.json();
+    console.log(data.results);
+}
+async function monstersData() {
+    const response = await fetch(monsters_url);
+    const data = await response.json();
+    console.log(data.results);
+}
 async function featuresData() {
-    const response = await fetch(features_api_url);
+    const response = await fetch(features_url);
     const data = await response.json();
     console.log(data.results);
-}}
+}
