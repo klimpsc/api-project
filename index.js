6d9ead423f59294b376bb7492a01693a3f1f059d
Seth Klimpsch <evangeling.klimpsch@gmail.com>
2024-04-15 10:12:13 +1000

    reverted back to earlier commit

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+let races_api_url = "https://www.dnd5eapi.co/api/races"
+let classes_api_url = "https://www.dnd5eapi.co/api/classes"
+let monsters_api_url = "https://www.dnd5eapi.co/api/monsters"
+let equiptment_api_url = "https://www.dnd5eapi.co/api/equiptment"
+let spells_api_url = "https://www.dnd5eapi.co/api/spells"
 
-async function fetchData(){
-    try {
-        const response = await fetch ("https://www.dnd5eapi.co/api/races");
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('ERROR!! ERROR!!', error);
-    }
-} 
+let features_api_url = "https://www.dnd5eapi.co/api/features"
+
+async function racesData() {
+    const response = await fetch(races_api_url);
+async function monsterData() {
+    console.log(data.results);
+}}
+
+async function equiptmentData() {
+    const response = await fetch(equipment_api_url);
+async function spellsData() {
+    const response = await fetch(spells_api_url);
+    const data = await response.json();
+    console.log(data.results);
+}}
+
+async function spellsData() {
+    const response = await fetch(spells_api_url);
+async function featuresData() {
+    const response = await fetch(features_api_url);
+    const data = await response.json();
+    console.log(data.results);
+}}
