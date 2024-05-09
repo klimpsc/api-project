81f07f2f436d22d4eddc27d74dddf3e075dd0cab
Seth Klimpsch <evangeling.klimpsch@gmail.com>
2024-05-09 19:05:36 +1000

    remove all functions except monstersData, for the sake of clarity. Will see about adding the others again later.

--- index.js
+++ index.js
@@ -6,35 +6,8 @@ let spells_url = "https://www.dnd5eapi.co/api/spells"
 let features_url = "https://www.dnd5eapi.co/api/features"
 
 
-
-async function rulesData() {
-    const response = await fetch(rules_url);
-    const data = await response.json();
-    console.log(data.results);
-}
-async function racesData() {
-    const response = await fetch(races_url);
-    const data = await response.json();
-    console.log(data.results);
-}
-async function classesData() {
-    const response = await fetch(classes_url);
-    const data = await response.json();
-    console.log(data.results);
-}
-async function spellsData() {
-    const response = await fetch(spells_url);
-    const data = await response.json();
-    console.log(data.results);
-}
 async function monstersData() {
     const response = await fetch(monsters_url);
     const data = await response.json();
     console.log(data.results);
 }
-async function featuresData() {
-    const response = await fetch(features_url);
-    const data = await response.json();
-    console.log(data.results);
-}
-
