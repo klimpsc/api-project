15ae8ba69c38550c1d544596596eb028b56d0471
Seth Klimpsch <evangeling.klimpsch@gmail.com>
2024-05-10 18:55:45 +1000

    Removed fetch data that I'm no longer using. Now am only fetching monster data with a single, less cluttered fetch

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-let rules_url = "https://www.dnd5eapi.co/api/rules"
-let races_url = "https://www.dnd5eapi.co/api/races"
-let classes_url = "https://www.dnd5eapi.co/api/classes"
-let monsters_url = "https://www.dnd5eapi.co/api/monsters"
-let spells_url = "https://www.dnd5eapi.co/api/spells"
-let features_url = "https://www.dnd5eapi.co/api/features"
+//"https://www.dnd5eapi.co/api/monsters"
 
-
-async function monstersData() {
-    const response = await fetch(monsters_url);
-    const data = await response.json();
-    console.log(data.results[4].name);
-    document.getElementById('root').
-    innerHTML= data.results[4].name;
-};
+fetch("https://www.dnd5eapi.co/api/monsters").then((data)=>{
+    //console.log(data);
+    return data.json();
+}).then((fetchedData)=>{
+    console.log(fetchedData)
+})
