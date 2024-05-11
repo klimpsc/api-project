d24c8488e160cd89119d3df94dcd7b873ebb8eda
Seth Klimpsch <evangeling.klimpsch@gmail.com>
2024-05-11 10:50:07 +1000

    Removed some html and css. Changed some js to be more clear

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 
 async function fetchMonsters(){
     const res = await fetch("https://www.dnd5eapi.co/api/monsters")
+    const {results} = await res.json()
+
+    renderMonster(results)
 }
-const result = fetchMonsters()
-console.log(result)
+
+
+
+function renderMonster(monsters) {
+    monsters.forEach(({ name }) => console.log(name))
+}
+
 
