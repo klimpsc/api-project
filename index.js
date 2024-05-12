3aa677e45fcf0578a763e6436fd374092447e7ca
Seth Klimpsch <evangeling.klimpsch@gmail.com>
2024-05-13 03:07:59 +1000

    rewriting html for the new templates

--- index.js
+++ index.js
@@ -1,56 +1,46 @@
-const currentPokemon = {}
-const searchField = document.querySelector('input')
-
-window.onload=function(){
-    searchField.addEventListener('change', () => {
-        const currentPokemonNames = Object.keys(currentPokemon)
-        const filteredPokemon = currentPokemonNames.filter(name => name.includes(searchField.value))
-        console.log(filteredPokemon)
-})
-}
-
-async function fetchPokemon(){
-    const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
-    const { results } = await res.json()
 
 
-    results.forEach(({ url }) => fetchPokemonDetails(url))
+
+
+
+
+// async function fetchMonsters(){
+//     const res = await fetch("https://www.dnd5eapi.co/api/monsters")
+//     const { results } = await res.json()
+
+//     results.forEach(({ url }) => fetchMonstersDetails(url))
         
-    }
+//     }
+
+// async function fetchMonstersDetails(url) {
+//     const res = await fetch(url)
+//     const pokemonData = await res.json()
+//     if(!currentMonsters[monsters.name])
+//         currentMonsters[monsters.name] = monstersData
+//     if(!currentMonsters[monsters.image])
+//         currentMonsters[monsters.image] = monsterData
+
+//     renderMonsters(monstersData)
+// }
+
+// function renderMonsters(monsters) {
+//     const container = document.querySelector('.container')
+//     const card = generateCard(monsters)
+//     container.append(card)
+// }
+
+// function generateCard(monsters) {
+//     const cardDiv = document.createElement('div')
+//     const image = document.createElement('img')
+//     cardDiv.className = 'card'
+//     image.src = monsters.image
+//     image.alt = monsters.name
+//     const title = document.createElement('h2')
+//     title.textContent = monsters.name
+//     cardDiv.append(image, title)
+
+//     return cardDiv 
+// }
 
 
-
-
-async function fetchPokemonDetails(url) {
-    const res = await fetch(url)
-    const pokemonData = await res.json()
-    if(!currentPokemon[pokemonData.name])
-        currentPokemon[pokemonData.name] = pokemonData
-
-    renderPokemon(pokemonData)
-}
-
-function renderPokemon(pokemon) {
-    const container = document.querySelector('.container')
-    const card = generateCard(pokemon)
-    container.append(card)
-}
-
-function generateCard(pokemon) {
-    const cardDiv = document.createElement('div')
-    const image = document.createElement('img')
-    cardDiv.className = 'card'
-    image.src = pokemon.sprites.other.dream_world.front_default
-    image.alt = pokemon.name
-    const title = document.createElement('h2')
-    title.textContent = pokemon.name
-    cardDiv.append(image, title)
-
-    return cardDiv 
-}
-
-function renderNavigation(next, previous) {
-    // Navigation for later-on
-}
-
-fetchPokemon()
+// fetchMonsters()
