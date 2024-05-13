5601b3fcffef20ab82c9d8ce1e821915136d4765
Seth Klimpsch <evangeling.klimpsch@gmail.com>
2024-05-13 19:57:03 +1000

    Rewriting lots of the html and css, and JS, breaking down each function in my head. Hopefully will clear up my code and my head. I hate Front-end stuff

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+async function fetchData() {
+    const response = await fetch('https://www.dnd5eapi.co/api/monsters/');
+    const monsters = await response.json();
+    console.log(monsters);
+}
+
+fetchData()
 
 
 
 
 
-
+///////////
 // async function fetchMonsters(){
 //     const res = await fetch("https://www.dnd5eapi.co/api/monsters")
 //     const { results } = await res.json()
