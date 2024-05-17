c62bc87300e6747aba0a7c77a281fb796119b10d
Seth Klimpsch <evangeling.klimpsch@gmail.com>
2024-05-17 11:54:32 +1000

    getting through Alberts recommended api lecture. Showing names, and displaying them

--- script.js
+++ script.js
@@ -1,52 +1,21 @@
 
-async function fetchData() {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon');
-    const {results} = await response.json();
-    results.forEach(({name}) => console.log(name))
+async function fetchPokemon() {
+    const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+    const { results } = await res.json()
+    renderPokemon(results)
 }
-const result = fetchData()
+function renderPokemon(pokemon) {
+    pokemon.forEach(({ name }) =>{
+        const container = document.querySelector('.container')
+        const card = generateCard(name)
+        container.append(card)
+    })
+}
+function generateCard(name){
+    const cardDiv = document.createElement('div')
+    cardDiv.textContent = name
 
+    return cardDiv
+}
 
-
-
-///////////
-// async function fetchMonsters(){
-//     const res = await fetch("https://www.dnd5eapi.co/api/monsters")
-//     const { results } = await res.json()
-
-//     results.forEach(({ url }) => fetchMonstersDetails(url))
-        
-//     }
-
-// async function fetchMonstersDetails(url) {
-//     const res = await fetch(url)
-//     const pokemonData = await res.json()
-//     if(!currentMonsters[monsters.name])
-//         currentMonsters[monsters.name] = monstersData
-//     if(!currentMonsters[monsters.image])
-//         currentMonsters[monsters.image] = monsterData
-
-//     renderMonsters(monstersData)
-// }
-
-// function renderMonsters(monsters) {
-//     const container = document.querySelector('.container')
-//     const card = generateCard(monsters)
-//     container.append(card)
-// }
-
-// function generateCard(monsters) {
-//     const cardDiv = document.createElement('div')
-//     const image = document.createElement('img')
-//     cardDiv.className = 'card'
-//     image.src = monsters.image
-//     image.alt = monsters.name
-//     const title = document.createElement('h2')
-//     title.textContent = monsters.name
-//     cardDiv.append(image, title)
-
-//     return cardDiv 
-// }
-
-
-// fetchMonsters()
+fetchPokemon()
