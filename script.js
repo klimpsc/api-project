c5e799d719933106691d38e8718bb8ac3004586c
Seth Klimpsch <evangeling.klimpsch@gmail.com>
2024-05-25 09:19:14 +1000

    Further writing alberts recommended code. Have my own preferred image size though. Am very sick😷

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 
 async function fetchPokemon() {
-    const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+    const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=30')
     const { results } = await res.json()
-    renderPokemon(results)
+    
+    results.forEach(({ url }) => fetchPokemonDetails(url))
 }
-function renderPokemon(pokemon) {
-    pokemon.forEach(({ name }) =>{
-        const container = document.querySelector('.container')
-        const card = generateCard(name)
-        container.append(card)
-    })
-}
-function generateCard(name){
-    const cardDiv = document.createElement('div')
-    cardDiv.textContent = name
 
+async function fetchPokemonDetails(url) {
+    const res = await fetch(url)
+    const pokemonData = await res.json()
+
+    renderPokemon(pokemonData)
+}
+
+
+function renderPokemon(pokemon) {
+    const container = document.querySelector('.container')
+    const card = generateCard(pokemon)
+    container.append(card)
+    
+}
+
+function generateCard(pokemon){
+    const cardDiv = document.createElement('div')
+    const image = document.createElement('img')
+    cardDiv.className = 'cards'
+    image.src = pokemon.sprites.other.dream_world.front_default
+    image.alt = pokemon.name
+    const title = document.createElement('h2')
+    title.textContent = pokemon.name
+    cardDiv.append( title, image)
     return cardDiv
 }
 
